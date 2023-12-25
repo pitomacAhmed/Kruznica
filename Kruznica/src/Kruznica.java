5c66db2082219e246e13ccd3e049f06b3e354ef7
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:31:42 +0100

    Add files via upload

---
+++ Kruznica/src/Kruznica.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Kruznica {
+	public static void main(String[] args) {
+    double radius, povrsina, perimetar;
+    double pi = 3.14;
+    Scanner tastatura = new Scanner(System.in);
+    System.out.println("Ovaa programa presmetuva perimetar i plostina na kruznica");
+    System.out.println("Vnesete go radiusot na kruznicata :");
+    radius = tastatura.nextDouble();
+    povrsina = radius*radius*pi;
+    perimetar = 2*radius*pi;
+    System.out.println();
+	System.out.println("Perimetarot e: " + perimetar);
+	System.out.println("Povrsinata e: " + povrsina);
+	}
+}
+
